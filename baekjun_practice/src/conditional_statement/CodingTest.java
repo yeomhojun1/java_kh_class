7423640aa2b95a260ba4316a79f7501ee0dba180
yeomhojun1 <135560342+yeomhojun1@users.noreply.github.com>
2023-11-03 15:23:04 +0900

    구구단 연습

--- baekjun_practice/src/conditional_statement/CodingTest.java
+++ baekjun_practice/src/conditional_statement/CodingTest.java
@@ -5,25 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CodingTest {
-	 static int[] min_arr(int[] A) {
-		 int min=A[0];
-		for(int i=0;i<A.length;i++) {
-			for(int j=i+1;j<A.length;j++) {
-			if(A[i]<A[j]) {
-				min=A[i];
-				A[i]=A[j];
-				A[j]=min;
-				
-			}}
-		}
-		return A;
-	 }
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		Long n= Long.parseLong(br.readLine());
-		System.out.println(n*n);
-		System.out.println('2');
-		System.out.println('3');
+		/*
+		 * BufferedReader br= new BufferedReader(new InputStreamReader(System.in)); Long
+		 * n= Long.parseLong(br.readLine()); System.out.println(n*n);
+		 */
+		for(int i =2;i<10;i++)
+			for(int j=1; j<10; j++)
+				System.out.println(i+"*"+j+"="+i*j);
+
 	}
 		
 	}

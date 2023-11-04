2290a84dea86fffac5bc9f6f631cd9a49b38c695
yeomhojun1 <135560342+yeomhojun1@users.noreply.github.com>
2023-11-04 17:29:45 +0900

    코딩테스트 연습

--- baekjun_practice/src/conditional_statement/CodingTest.java
+++ baekjun_practice/src/conditional_statement/CodingTest.java
@@ -5,7 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CodingTest {
+	 public String solution(String s) {
+	        String answer = "";
+	        
+	        String[] numbers = s.split(" ");
+	        int min = Integer.MAX_VALUE;
+	        int max = Integer.MIN_VALUE;
+	        
+	        for(int i = 0; i < numbers.length; i++){
+	            int number = Integer.parseInt(numbers[i]);
+	            
+	            min = Math.min(min, number);
+	            max = Math.max(max, number);
+	        }
 
+	        answer = min+ " " +max;
+	        return answer;
+	    }
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		/*
 		 * BufferedReader br= new BufferedReader(new InputStreamReader(System.in)); Long

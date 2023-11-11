f25203d2f72318140a32b597bfa33b5a6820dfae
yeomhojun1 <135560342+yeomhojun1@users.noreply.github.com>
2023-11-11 23:48:07 +0900

    코딩테스트 연습

--- baekjun_practice/src/conditional_statement/CodingTest.java
+++ baekjun_practice/src/conditional_statement/CodingTest.java
@@ -5,33 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CodingTest {
-	 public String solution(String s) {
-	        String answer = "";
-	        
-	        String[] numbers = s.split(" ");
-	        int min = Integer.MAX_VALUE;
-	        int max = Integer.MIN_VALUE;
-	        
-	        for(int i = 0; i < numbers.length; i++){
-	            int number = Integer.parseInt(numbers[i]);
-	            
-	            min = Math.min(min, number);
-	            max = Math.max(max, number);
+	 public String solution(String str) {
+	      
+	        String[] tmp = str.split(" ");
+	        int min, max, n;
+	        min = max = Integer.parseInt(tmp[0]);
+	        for (int i = 1; i < tmp.length; i++) {
+	                n = Integer.parseInt(tmp[i]);
+	            if(min > n) min = n;
+	            if(max < n) max = n;
 	        }
-
-	        answer = min+ " " +max;
-	        return answer;
-	    }
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		/*
-		 * BufferedReader br= new BufferedReader(new InputStreamReader(System.in)); Long
-		 * n= Long.parseLong(br.readLine()); System.out.println(n*n);
-		 */
-		for(int i =2;i<10;i++)
-			for(int j=1; j<10; j++)
-				System.out.println(i+"*"+j+"="+i*j);
-
-	}
-		
+	 
+	        return min + " " + max;
+	 
+	  }
 	}
 
